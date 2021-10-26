dc2943cbc7c2f28eb2f470ad0b426ed0262382a8
Betoloti24 <roiberthgonzalez091d@gmail.com>
2021-10-26 09:42:37 -0400

    Desarrollo 2

--- HAROLD/settings.py
+++ HAROLD/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k13r0hx*tdgvkiytv%%@v=b&wztjm!-8=)+za_37rg2kmm3_2s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
